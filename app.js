a1b8f764606d957eff8eb85914241441db8fa7ce
usman_rizwan <usmanrizwan771@gmail.com>
2023-11-23 20:44:50 +0500

    time format update with some css changes.

--- app.js
+++ app.js
@@ -28,22 +28,23 @@ function displayTime() {
 setInterval(displayTime, 1000);
 let fajr = "05:25:00";
 let zuhr = "12:17:00";
-let asr = "16:07:00";
-let maghrib = "17:43:00";
-let isha = "19:02:00";
+let asr = "04:07:00";
+let maghrib = "05:43:00";
+let isha = "07:02:00";
 
 function showPrayers() {
-    const newTime = moment().format("HH:mm:ss");
+    const newTime = moment().format("LTS");
+    // console.log(newTime);    
     if (newTime >= fajr && newTime < zuhr) {
-        namazAfter.innerHTML = `Next Prayer Will Be  <span class="cl-g "> Zuhr </span> At  <h3 class="cl-g clock">${zuhr} pm </h3>`;
+        namazAfter.innerHTML = `Next Prayer Will Be  <span class="cl-g "> Zuhr </span> At  <h3 class="cl-g clock">${zuhr} PM </h3>`;
     } else if (newTime >= zuhr && newTime < asr) {
-        namazAfter.innerHTML = `Next Prayer Will Be <span class="cl-g "> Asr </span> At  <h3 class="cl-g clock">${asr} pm </h3>`;
+        namazAfter.innerHTML = `Next Prayer Will Be <span class="cl-g "> Asr </span> At  <h3 class="cl-g clock">${asr} PM </h3>`;
     } else if (newTime >= asr && newTime < maghrib) {
-        namazAfter.innerHTML = `Next Prayer Will Be  <span class="cl-g "> Maghrib </span> At   <h3 class="cl-g clock">${maghrib} pm </h3>`;
+        namazAfter.innerHTML = `Next Prayer Will Be  <span class="cl-g "> Maghrib </span> At   <h3 class="cl-g clock">${maghrib} PM </h3>`;
     } else if (newTime >= maghrib && newTime < isha) {
-        namazAfter.innerHTML = `Next Prayer Will Be  <span class="cl-g "> Isha </span> At   <h3 class="cl-g clock">${isha} pm </h3> `;
+        namazAfter.innerHTML = `Next Prayer Will Be  <span class="cl-g "> Isha </span> At   <h3 class="cl-g clock">${isha} PM </h3> `;
     } else {
-        namazAfter.innerHTML = `Next Prayer Will Be <span class="cl-g "> Fajr </span> At <h3 class="cl-g clock">${fajr} am </h3> `;
+        namazAfter.innerHTML = `Next Prayer Will Be <span class="cl-g "> Fajr </span> At <h3 class="cl-g clock">${fajr} AM </h3> `;
     }
 }
 setInterval(showPrayers, 1000);
